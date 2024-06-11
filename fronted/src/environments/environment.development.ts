1c8e51c3b012adec3ec24799c9205ca6a4dd6c78
jordivillar10 <167296899+jordivillar10@users.noreply.github.com>
2024-06-11 19:32:54 +0200

    cmabios https

--- fronted/src/environments/environment.development.ts
+++ fronted/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     stripe_pk: 'pk_test_51PHOSuDJExqBi5rheurIyddSRqnJIEwHsw7ic7HEvgHWbpFjZFOh98VY7z4n0cLk62sfjauLuIO1tfTxKCSjaYYn00wZM5HcO3',
-    endpoint: 'http://44.203.197.175:3001/'
+    endpoint: 'https://52.87.175.172:3001/'
 };
