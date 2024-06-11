6497e2bbe880db38a806908e95471f6ef45c83d3
jordivillar10 <167296899+jordivillar10@users.noreply.github.com>
2024-06-11 18:01:15 +0200

    conexion cambio a remoto

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize('rrhh', 'root', 'Nohay2sin3', {
-    host: 'localhost',
+var sequelize = new sequelize_1.Sequelize('koliseo', 'jordi', 'Admin123', {
+    host: '54.226.65.179',
     dialect: 'mysql',
 });
 exports.default = sequelize;
